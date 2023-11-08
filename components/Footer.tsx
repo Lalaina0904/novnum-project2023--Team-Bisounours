fc526cf8e37df9f9a3c4fa8c6469c78aa953db06
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-11-08 17:37:20 +0300

    🐛 feat : ... homepage features

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,131 +1,129 @@
 import * as React from "react";
 import {
-    AiFillFacebook,
-    AiFillInstagram,
-    AiFillLinkedin,
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
 } from "react-icons/ai";
 
 const Footer = () => {
-    return (
-        <footer className="bg-slate-200">
-            <div className="container mx-auto px-6">
-                <div className="md:grid grid-cols-4 gap-5 py-8 text-gray-600">
-                    <div className="col-span-2">
-                        <p className="text-3xl">Newsletter</p>
-                        <p className="my-4">
-                            Subscribe to our newsletter and get our newest
-                            updates right on your inbox.
-                        </p>
-                        <div>
-                            <input
-                                type="email"
-                                placeholder="Enter your email"
-                                name="email"
-                                className="outline-nones border border-gray-400 w-1/2 pr-2 pl-2 py-3 mt-2 rounded-r-none rounded-md text-gray-800 font-semibold"
-                            />
-                            <button className="inline-flex items-center px-6 py-3 mt-2 transition duration-500 ease-in-out transform border rounded-l-none rounded-lg bg-gray-900">
-                                <a className="font-medium text-white" href="/">
-                                    <span className="justify-center">
-                                        Subscribe
-                                    </span>
-                                </a>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div className="my-4 lg:my-0">
-                        <p className="font-semibold text-2xl">Need help?</p>
-                        <p>
-                            <a href="#" className="text-[0.8rem] text-gray-600">
-                                Manage my bookings
-                            </a>
-                        </p>
-
-                        <p>
-                            <a href="#" className="text-[0.8rem] text-gray-600">
-                                Assistance
-                            </a>
-                        </p>
-
-                        <p>
-                            <a href="#" className="text-[0.8rem] text-gray-600">
-                                Best price guaranteed
-                            </a>
-                        </p>
-                    </div>
-
-                    <div className="my-4 lg:my-0">
-                        <p className="font-semibold text-2xl">Social medias </p>
-
-                        <div className="flex mt-4 gap-4">
-                            <a href="">
-                                <AiFillFacebook className="text-2xl" />
-                            </a>
-                            <a href="">
-                                <AiFillInstagram className="text-2xl" />
-                            </a>
-                            <a href="">
-                                <AiFillLinkedin className="text-2xl" />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex flex-col-reverse justify-between pt-5 pb-4 border-t lg:flex-row bg-top border-gray-400 mt-5">
-                    <ul className="flex flex-col space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-                        <li>
-                            <a
-                                href="/"
-                                className="text-sm text-gray-700 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                            >
-                                Terms of Service
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                className="text-sm text-gray-700 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                            >
-                                Privacy Policy
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                className="text-sm text-gray-700 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                            >
-                                Ad Choices
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                className="text-sm text-gray-700 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                            >
-                                Cookie Policy
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="/"
-                                className="text-sm text-gray-700 transition-colors duration-300 hover:text-deep-purple-accent-400"
-                            >
-                                Partners
-                            </a>
-                        </li>
-                    </ul>
-                    <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
-                        <a
-                            href="/"
-                            className="text-sm text-gray-700 transition-colors duration-300 hover:text-deep-purple-accent-400 tracking-tight"
-                        >
-                            © 2023 NovNum Inc.
-                        </a>
-                    </ul>
-                </div>
+  return (
+    <footer className="bg-slate-200">
+      <div className="container mx-auto px-6">
+        <div className="grid-cols-4 gap-5 py-8 text-gray-600 md:grid">
+          <div className="col-span-2">
+            <p className="text-3xl">Newsletter</p>
+            <p className="my-4">
+              Subscribe to our newsletter and get our newest updates right on
+              your inbox.
+            </p>
+            <div>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                name="email"
+                className="outline-nones mt-2 w-1/2 rounded-md rounded-r-none border border-gray-400 py-3 pl-2 pr-2 font-semibold text-gray-800"
+              />
+              <button className="mt-2 inline-flex transform items-center rounded-lg rounded-l-none border bg-gray-900 px-6 py-3 transition duration-500 ease-in-out">
+                <a className="font-medium text-white" href="/">
+                  <span className="justify-center">Subscribe</span>
+                </a>
+              </button>
             </div>
-        </footer>
-    );
+          </div>
+
+          <div className="my-4 lg:my-0">
+            <p className="text-2xl font-semibold">Need help?</p>
+            <p>
+              <a href="#" className="text-[0.8rem] text-gray-600">
+                Manage my account
+              </a>
+            </p>
+
+            <p>
+              <a href="#" className="text-[0.8rem] text-gray-600">
+                Assistance
+              </a>
+            </p>
+
+            <p>
+              <a href="#" className="text-[0.8rem] text-gray-600">
+                Best price guaranteed
+              </a>
+            </p>
+          </div>
+
+          <div className="my-4 lg:my-0">
+            <p className="text-2xl font-semibold">Social medias </p>
+
+            <div className="mt-4 flex gap-4">
+              <a href="">
+                <AiFillFacebook className="text-2xl" />
+              </a>
+              <a href="">
+                <AiFillInstagram className="text-2xl" />
+              </a>
+              <a href="">
+                <AiFillLinkedin className="text-2xl" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5 flex flex-col-reverse justify-between border-t border-gray-400 bg-top pb-4 pt-5 lg:flex-row">
+          <ul className="flex flex-col space-y-2 sm:flex-row sm:space-x-5 sm:space-y-0 lg:mb-0">
+            <li>
+              <a
+                href="/"
+                className="hover:text-deep-purple-accent-400 text-sm text-gray-700 transition-colors duration-300"
+              >
+                Terms of Service
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="hover:text-deep-purple-accent-400 text-sm text-gray-700 transition-colors duration-300"
+              >
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="hover:text-deep-purple-accent-400 text-sm text-gray-700 transition-colors duration-300"
+              >
+                Ad Choices
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="hover:text-deep-purple-accent-400 text-sm text-gray-700 transition-colors duration-300"
+              >
+                Cookie Policy
+              </a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className="hover:text-deep-purple-accent-400 text-sm text-gray-700 transition-colors duration-300"
+              >
+                Partners
+              </a>
+            </li>
+          </ul>
+          <ul className="mb-3 flex flex-col space-y-2 sm:flex-row sm:space-x-5 sm:space-y-0 lg:mb-0">
+            <a
+              href="/"
+              className="hover:text-deep-purple-accent-400 text-sm tracking-tight text-gray-700 transition-colors duration-300"
+            >
+              © 2023 NovNum Inc.
+            </a>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/home/Heading.tsx
+++ components/home/Heading.tsx
@@ -2,40 +2,49 @@ import * as React from "react";
 import Image from "next/image";
 
 export const Heading = () => {
-    return (
-        <header className="bg-primary-dark flex flex-col items-center h-[90vh] mb-24">
-            <section className="text-primary-light container grow items-center mx-auto flex px-2 md:py-[64px] lg:py-[96px] z-[1]">
-                <div className="w-full lg:w-2/3">
-                    <h1 className="text-4xl md:text-5xl mb-6 font-semibold">
-                        Explore a world of collaborative learning where your
-                        skills grow through sharing
-                    </h1>
-                    <h2 className="font-alata text-2xl mb-4 w-4/5 md:py-8">
-                        We are focused on a collective intelligence approach
-                    </h2>
-                    <p className="text-lg mb-32 md:mb-0 opacity-50">
-                        We promote collaboration, sharing, and co-creation of
-                        educational resources, placing the human element at the
-                        center of our approach to develop the education sector
-                        in Madagascar. Our goal is to create a collaborative
-                        learning environment where students, teachers, and
-                        education experts come together to revolutionize
-                        education in Madagascar by harnessing the collective
-                        wisdom of our educational community
-                    </p>
-                </div>
-                <div className="w-2/5 justify-center hidden lg:flex invert-0 animate-spinAnime">
-                    {/* <Image
-                        className="invert"
-                        width={250}
-                        height={200}
-                        src={"/image/spinner.svg"}
-                        alt={"spinner image"}
-                    /> */}
-                </div>
-            </section>
-        </header>
-    );
+  return (
+    <header className="bg-primary-dark mb-24 flex h-[90vh] flex-col items-center">
+      <section className="text-primary-light container z-[1] mx-auto flex grow items-center px-2 md:py-[64px] lg:py-[96px]">
+        <div className="w-full lg:w-2/3">
+          <h1 className="mb-8 text-[2.4rem] font-semibold leading-[3.6rem] md:text-[3rem]">
+            <span className="bg-gradient-to-r from-[#fab99b] via-[#c3aeff] to-[#7bb1e4]  bg-clip-text leading-[2rem] text-transparent">
+              Explore a world{" "}
+            </span>
+            <span className="leading-[2rem] text-[#477cad]">
+              {" "}
+              of collaborative learning where your skills grow through sharing
+            </span>
+          </h1>
+          <h2 className="font-alata mb-6 w-4/5 text-[1.6rem] text-gray-800 md:py-8">
+            We are focused on a collective intelligence approach
+          </h2>
+          <p className="mb-16 opacity-60 lg:mb-32 ">
+            We promote collaboration, sharing, and co-creation of educational
+            resources, placing the human element at the center of our approach
+            to develop the education sector in Madagascar. Our goal is to create
+            a collaborative learning environment where students, teachers, and
+            education experts come together to revolutionize education in
+            Madagascar by harnessing the collective wisdom of our educational
+            community
+          </p>
+          <button>
+            <a href="#about">Learn More</a>
+          </button>
+        </div>
+        <div className="relative hidden h-full w-2/5 justify-center lg:flex">
+          <Image
+            className="heroImg absolute top-[2rem]"
+            src="/images/hero-img.png"
+            alt={"hero-img"}
+            layout="responsive"
+            objectFit="contain"
+            width={500}
+            height={500}
+          />
+        </div>
+      </section>
+    </header>
+  );
 };
 
 export default Heading;
